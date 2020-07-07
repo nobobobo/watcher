9376a8a866e2db9636cc6c3328ce695d46d1401f
nobobobo <ianhsu1221@gmail.com>
2020-07-06 21:35:08 -0700

    CRUD for API, get, post

---
+++ routes/api-routes.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+const { sequelize, literal, Op } = require('sequelize');
+const moment = require('moment');
+
+module.exports = function (app) {
+    app.get("/api/feeds", (req, res) => {
+        const userlat = req.body.lat;
+        const userlng = req.body.lng;
+
+        // SELECT * FROM Posts WHERE DATEDIFF(CURDATE, createdAt) <= 7 ORDER BY POWER((lat-userLat),2) + POWER((lng-userlng),2) ASC;
+        db.Post.findAll({
+            where: [{
+                createdAt: {
+                    [Op.gt]: moment().subtract(7, 'days').toDate()
+                }
+            }],
+            order: [
+                [literal(`POWER((lat-${userlat}),2) + POWER((lng-${userlng}),2) ASC`)]
+            ]
+        }
+        ).then(feed => res.json(feed));
+    });
+
+    app.post("/api/feeds", (req, res) => {
+
+        // req.body format: 
+        // {
+        //     userName: "Nobo",
+        //     body: "HELP!",
+        //     lat: 39.807222,
+        //     lng: -76.984722
+        // }
+
+        db.Post.create(req.body
+
+        ).then(feed => res.json(feed));
+    })
+
+}
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("view engine", "handlebars");
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
-//require("./routes/author-api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 //require("./routes/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
@@ -39,3 +39,14 @@ db.sequelize.sync({ force: true }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+
+// var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyC5VP3pybEc9I2lF4vK1vcJpsYtraibm68&callback=initMap";
+// $(document).ready(function () {
+//     $.ajax({
+//         url: queryURL,
+//         method: "POST"
+//     }).then(function (response) {
+//         console.log(response);
+//     });
+// }); 
